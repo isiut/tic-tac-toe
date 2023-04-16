566e28b317aa676d13058fe2d18ebb2693a8892a
Illia Siutkin <illiasiutkin07@gmail.com>
2023-04-16 17:51:08 -0500

    finish win checker

--- src/main.c
+++ src/main.c
@@ -76,6 +76,50 @@ int handleInput(int player)
 
 int winner = 0;
 
+int checkWin(void)
+{
+    // Horizontal combination
+    int h1 = board[0] + board[1] + board[2];
+    int h2 = board[3] + board[4] + board[5];
+    int h3 = board[6] + board[7] + board[8];
+
+    // Vertical combination
+    int v1 = board[0] + board[3] + board[6];
+    int v2 = board[1] + board[4] + board[7];
+    int v3 = board[2] + board[5] + board[8];
+
+    // Diagonal combination
+    int d1 = board[0] + board[4] + board[8];
+    int d2 = board[2] + board[4] + board[6];
+
+    int filledSquares = 0;
+    for (int i = 0; i <= 8; i++)
+    {
+        if (board[i] != 0)
+        {
+            filledSquares++;
+        }
+    }
+
+    if (h1 == 3 || h2 == 3 || h3 == 3 || v1 == 3 || v2 == 3 || v3 == 3 || d1 == 3 || d2 == 3)
+    {
+        printf("x won!\n");
+        return 1;
+    }
+    else if (h1 == -3 || h2 == -3 || h3 == -3 || v1 == -3 || v2 == -3 || v3 == -3 || d1 == -3 || d2 == -3)
+    {
+        printf("o won!\n");
+        return -1;
+    }
+    else if (filledSquares >= 9)
+    {
+        printf("It's a draw!\n");
+        return -2;
+    }
+
+    return 0;
+}
+
 int main(void)
 {
     printf("--Tic-Tac-Toe--\n");
@@ -91,6 +135,10 @@ int main(void)
             printf("Invalid input\n");
             return -1;
         }
+        if (checkWin() != 0)
+        {
+            break;
+        }
 
         handleInput(-1);
         printBoard(board);
@@ -98,6 +146,10 @@ int main(void)
         {
             printf("Invalid input\n");
             return -1;
+        }
+        if (checkWin() != 0)
+        {
+            break;
         }
     }
 
